71c9ca26c7a36a5a7662992322b2368b363c1df7
NickMoignard <nicklmoignard@gmail.com>
2023-07-06 14:52:31 +1000

    add postgres to template

--- src/app/module.ts
+++ src/app/module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controller';
 import { AppService } from './service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfigService } from '../database/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
